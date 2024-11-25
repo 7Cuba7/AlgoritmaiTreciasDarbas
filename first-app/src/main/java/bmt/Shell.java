a1fb32bba6cdba858e27a5428885e6333ead5054
7Cuba7 <tauras.tauriukas@gmail.com>
2024-11-26 00:30:21 +0200

    Update Shell.java with added numbers reader from file

--- first-app/src/main/java/bmt/Shell.java
+++ first-app/src/main/java/bmt/Shell.java
@@ -1,8 +1,16 @@
 package bmt;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Shell {
 
-  int sort(int arr[]) {
+  static int sort(int arr[]) {
     int n = arr.length;
     for (int gap = n / 2; gap > 0; gap = gap / 2) {
       for (int i = gap; i < n; i = i + 1) {
@@ -24,10 +32,32 @@ public class Shell {
     System.out.println();
   }
 
-  public void core() {
-    int arr[] = { 12, 34, 54, 2, 3 }; // Skaiciu sarasas
-    System.out.println("Array before sorting");
-    printArray(arr);
+  private static int[] readNumbersFromFile(String fileName) throws IOException {
+    List<Integer> numberList = new ArrayList<>();
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        numberList.add(Integer.parseInt(line.trim()));
+      }
+    }
+
+    return numberList.stream().mapToInt(Integer::intValue).toArray();
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[0];
+    String fileName = "random_10k.txt";
+
+    try {
+      arr = readNumbersFromFile(fileName);
+    } catch (IOException e) {
+      System.out.println("An error occurred: " + e.getMessage());
+      return;
+    }
+
+    // System.out.println("Array before sorting");
+    // printArray(arr);
 
     sort(arr);
 
